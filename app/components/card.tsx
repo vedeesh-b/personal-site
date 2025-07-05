fae1ac5ace334697c25c788a559e68d78d454003
vedeesh-b <vedeesh18@gmail.com>
2025-07-05 13:21:56 +0100

    fix project cards

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -22,7 +22,11 @@ export default function Card({ layout, src, alt }: CardProps) {
         {layout.key && <div className="caption">[ {layout.key} ]</div>}
         {layout.title && <div className="title">{layout.title}</div>}
         {layout.text && <div>{layout.text}</div>}
-        {layout.btn?.text && <button>{layout.btn.text}</button>}
+        {layout.btn?.text && (
+          <a role="button" href={layout.btn.href} className="nav-btn">
+            {layout.btn.text}
+          </a>
+        )}
       </div>
       <img src={src} alt={alt} height="200px" />
     </div>
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,23 +3,23 @@ import "./components.css";
 export default function Navbar() {
   const navbarLinks = [
     {
-      title: 'About',
-      path: '/about'
+      title: "About",
+      path: "/about",
     },
     {
-      title: 'Projects',
-      path: '/projects'
-    }
+      title: "Projects",
+      path: "/projects",
+    },
   ];
   return (
     <div id="navbar-container">
-      <div id="headshot-img">Logo</div>
+      <div id="headshot-img">
+        <a href="/">Logo</a>
+      </div>
       <div id="links">
         {navbarLinks.map((link, key) => (
           <div className={`${link.title}-link`} key={key}>
-            <a href={link.path}>
-              {link.title}
-            </a>
+            <a href={link.path}>{link.title}</a>
           </div>
         ))}
       </div>
